5fa02571068ec8b397a6a6c783d49a564a6c798d
huajian zhang <278554189@qq.com>
2020-08-02 18:21:31 +0800

    添加项目文件。

---
+++ ContusoUniversity/Data/SchoolContext.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ContusoUniversity.Models;
+
+namespace ContusoUniversity.Data
+{
+    public class SchoolContext : DbContext
+    {
+        public SchoolContext (DbContextOptions<SchoolContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Enrollment> Enrollments { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Student>().ToTable("Student");
+            modelBuilder.Entity<Course>().ToTable("Course");
+            modelBuilder.Entity<Enrollment>().ToTable("Enrollment");
+        }
+    }
+}
---
+++ ContusoUniversity/Models/CourseAssignment.cs
@@ -0,0 +1,11 @@
+﻿namespace ContusoUniversity.Models
+{
+    public class CourseAssignment
+    {
+        public int InstructorID { get; set; }
+        public int CourseID { get; set; }
+
+        public Instructor Instructor { get; set; }
+        public Course Course { get; set; }
+    }
+}
