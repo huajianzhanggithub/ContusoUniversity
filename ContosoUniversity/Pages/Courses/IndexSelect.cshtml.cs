fa855e1f3497716596fc194e3278801ce01330b0
huajian zhang <278554189@qq.com>
2020-08-05 18:09:21 +0800

    2020年8月5日 18点09分

---
+++ ContosoUniversity/Pages/Courses/IndexSelect.cshtml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ContosoUniversity.Data;
+using ContosoUniversity.Models.SchoolViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+
+namespace ContosoUniversity.Pages.Courses
+{
+    public class IndexSelectModel : PageModel
+    {
+
+        private readonly SchoolContext _context;
+
+        public IndexSelectModel(SchoolContext context)
+        {
+            _context = context;
+        }
+        public IList<CourseViewModel> CourseVM { get; set; }
+
+        public async Task OnGetAsync()
+        {
+            CourseVM = await _context.Courses
+                .Select(p => new CourseViewModel
+                {
+                    CourseID = p.CourseID,
+                    Title = p.Title,
+                    Credits = p.Credits,
+                    DepartmentName = p.Department.Name
+                }).ToListAsync();
+        }
+    }
+}
